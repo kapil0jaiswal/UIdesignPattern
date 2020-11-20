4cd6a9f7fe2ee8a9e211fff47d7239f4d37b8187
kapil0jaiswal <kapiljaiswal82@gmail.com>
2020-11-20 15:03:53 +0530

    nav tag

---
+++ nav-tab/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TabNAv from './components/navtab/TabNAv';
+import Navigation from './components/navtab/Navigation';
+
+function App() {
+  return (
+    <div className="App ">
+     <Navigation/>
+    </div>
+  );
+}
+
+export default App;
